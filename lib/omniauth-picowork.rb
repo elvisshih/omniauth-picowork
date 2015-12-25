5dcc6a9953319f4de0a8791e0cc3b3b03906647b
elvis <elvis@u3dspace.com>
2015-12-25 19:55:37 +0800

    Add strategies for support server and modify for dev server

--- lib/omniauth-picowork.rb
+++ lib/omniauth-picowork.rb
@@ -1,2 +1,3 @@
 require "omniauth-picowork/version"
 require "omniauth/strategies/picowork"
+require "omniauth/strategies/picowork_support"
--- lib/omniauth/strategies/picowork.rb
+++ lib/omniauth/strategies/picowork.rb
@@ -11,15 +11,20 @@ module OmniAuth
       # initializing your consumer from the OAuth gem.
       option :client_options, {
         :site => "https://dev.picowork.com",
-        :authorize_url => "/uAuth/oauth2/authorize"
+        :authorize_url => "/uAuth/oauth2/authorize",
+        :token_url => "https://dev.picowork.com/uAuth/oauth2/token"
       }
+
+      option :scope, 'r_basicprofile r_emailaddress'
+      option :fields, ['user_id']
 
       # These are called after authentication has succeeded. If
       # possible, you should try to set the UID without making
       # additional calls (if the user id is returned with the token
       # or as a URI parameter). This may not be possible with all
       # providers.
-      uid{ request.params['user_id'] }
+      # uid{ request.params['user_id'] }
+      uid { raw_info['user_id'] }
 
       info do
         {
@@ -34,15 +39,49 @@ module OmniAuth
         }
       end
 
+      def callback_phase
+        with_authorization_code! do
+          super
+        end
+      rescue NoAuthorizationCodeError => e
+        fail!(:no_authorization_code, e)
+      rescue OmniAuth::Facebook::SignedRequest::UnknownSignatureAlgorithmError => e
+        fail!(:unknown_signature_algorithm, e)
+      end
+      
+      alias :oauth2_access_token :access_token
+
+      def access_token
+        ::OAuth2::AccessToken.new(client, oauth2_access_token.token, {
+          :mode => :query,
+          :param_name => 'oauth2_access_token',
+          :expires_in => oauth2_access_token.expires_in,
+          :expires_at => oauth2_access_token.expires_at
+        })
+      end
+
       def raw_info
-        @raw_info ||= JSON.load(access_token.get('/me.json')).body
+        @raw_info ||=  access_token.get("/uHutt/contact/:(#{option_fields.join(',')})?format=json").parsed
       end
 
       def authorize_params
         super.tap do |params|
           params[:response_type] ||= 'code'
-          params[:redirect_uri] ||= "https://pbc-staging.picowork.com/auth/picowork/callback"
+          params[:redirect_uri] ||= options.redirect_uri
         end
+      end
+
+      private
+
+      def option_fields
+        fields = options.fields
+        fields.map! { |f| f == "picture-url" ? "picture-url;secure=true" : f } if !!options[:secure_image_url]
+        fields
+      end
+
+      def user_name
+        name = "#{raw_info['firstName']} #{raw_info['lastName']}".strip
+        name.empty? ? nil : name
       end
     end
 
