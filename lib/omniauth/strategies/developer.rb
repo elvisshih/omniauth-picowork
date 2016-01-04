fdc3b3197f52d85b4f06d7c234b1a18ac93aa1b0
elvis <elvis@u3dspace.com>
2016-01-04 21:17:05 +0800

    refactor developer strategies

--- lib/omniauth/strategies/developer.rb
+++ lib/omniauth/strategies/developer.rb
@@ -1,6 +1,5 @@
 require 'json'
 require 'omniauth-oauth2'
-
 module OmniAuth
   module Strategies
     class Developer < OmniAuth::Strategies::OAuth2
@@ -11,78 +10,60 @@ module OmniAuth
       # initializing your consumer from the OAuth gem.
       option :client_options, {
         :site => "https://developer.picowork.com",
-        :authorize_url => "/uAuth/oauth2/authorize",
+        :authorize_url => "https://developer.picowork.com/uAuth/oauth2/authorize",
         :token_url => "https://developer.picowork.com/uAuth/oauth2/token"
       }
 
-      option :scope, 'r_basicprofile r_emailaddress'
-      option :fields, ['user_id']
+      #option :scope, 'r_basicprofile r_emailaddress'
+      # option :fields, ['user_id', 'email']
 
       # These are called after authentication has succeeded. If
       # possible, you should try to set the UID without making
       # additional calls (if the user id is returned with the token
       # or as a URI parameter). This may not be possible with all
       # providers.
-      # uid{ request.params['user_id'] }
       uid { raw_info['user_id'] }
 
       info do
         {
-          :name => raw_info['name'],
-          :location => raw_info['city']
+          :nickname => raw_info['nickname'],
+          :email => raw_info['email'],
+          :image_url => raw_info["image_url"]
         }
       end
 
       extra do
-        {
-          'raw_info' => raw_info
-        }
+        { 'raw_info' => raw_info }
       end
 
-      def callback_phase
-        with_authorization_code! do
-          super
-        end
-      rescue NoAuthorizationCodeError => e
-        fail!(:no_authorization_code, e)
-      rescue OmniAuth::Facebook::SignedRequest::UnknownSignatureAlgorithmError => e
-        fail!(:unknown_signature_algorithm, e)
-      end
-      
-      alias :oauth2_access_token :access_token
-
-      def access_token
-        ::OAuth2::AccessToken.new(client, oauth2_access_token.token, {
-          :mode => :query,
-          :param_name => 'oauth2_access_token',
-          :expires_in => oauth2_access_token.expires_in,
-          :expires_at => oauth2_access_token.expires_at
-        })
+      def build_access_token
+        verifier = request.params["code"]
+        client.auth_code.get_token(verifier, {:redirect_uri => options.redirect_uri}.merge(token_params.to_hash(:symbolize_keys => true)), deep_symbolize(options.auth_token_params))
       end
 
       def raw_info
-        @raw_info ||=  access_token.get("/uHutt/contact/:(#{option_fields.join(',')})?format=json").parsed
-      end
-
-      def authorize_params
-        super.tap do |params|
-          params[:response_type] ||= 'code'
-          params[:redirect_uri] ||= options.redirect_uri
+        path = options.client_options.site + '/uHutt/account/info'
+        uri = URI(path)
+        res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+          req = Net::HTTP::Get.new(uri.path)
+          req.add_field 'authorization', access_token.token
+          http.request req
         end
+        @raw_info ||= MultiJson.decode(res.body)['result']
       end
 
-      private
+    #   private
 
-      def option_fields
-        fields = options.fields
-        fields.map! { |f| f == "picture-url" ? "picture-url;secure=true" : f } if !!options[:secure_image_url]
-        fields
-      end
+    #   def option_fields
+    #     fields = options.fields
+    #     fields.map! { |f| f == "picture-url" ? "picture-url;secure=true" : f } if !!options[:secure_image_url]
+    #     fields
+    #   end
 
-      def user_name
-        name = "#{raw_info['firstName']} #{raw_info['lastName']}".strip
-        name.empty? ? nil : name
-      end
+    #   def user_name
+    #     name = "#{raw_info['firstName']} #{raw_info['lastName']}".strip
+    #     name.empty? ? nil : name
+    #   end
     end
   end
 end
